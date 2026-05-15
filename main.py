2610d7b88e8f981dd1d0568dfdec21b5ac8c46cf
benjasmines <benjaminuribeg@gmail.com>
2026-05-15 00:09:03 -0400

    feat funcó la prueba

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from jinja2 import Enviroment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
     loader=PackageLoader("test"),
